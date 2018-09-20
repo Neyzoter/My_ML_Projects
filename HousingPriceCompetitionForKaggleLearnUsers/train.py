5edba91bf9d3026612b0a4ac4a5b756548a20429
Neyzoter <sonechaochao@gmail.com>
2018-09-20 21:28:25 +0800

    add sub2

--- HousingPriceCompetitionForKaggleLearnUsers/train.py
+++ HousingPriceCompetitionForKaggleLearnUsers/train.py
@@ -13,6 +13,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing.imputation import Imputer
 
 
 train_data = pd.read_csv("./train.csv")
@@ -25,24 +26,52 @@ train_features = train_data.drop(['Id','SalePrice'],axis = 1)
 # 获取测试集
 test_features = test_data.drop(['Id'],axis = 1)
 
-# 获取有缺失值的列
+## 获取有缺失值的列
+#missing_cols_train = [col for col in train_features.columns
+#                     if train_features[col].isnull().any()]
+#
+#missing_cols_test = [col for col in test_features.columns
+#                     if test_features[col].isnull().any()]
+#
+###查找test和train中包含NaN的列有什么不同
+##[col for col in missing_cols_train if col not in missing_cols_test]
+#'''
+#test NaN 而train没有的
+#['MSZoning',
+# 'Utilities',
+# 'Exterior1st',
+# 'Exterior2nd',
+# 'BsmtFinSF1',
+# 'BsmtFinSF2',
+# 'BsmtUnfSF',
+# 'TotalBsmtSF',
+# 'BsmtFullBath',
+# 'BsmtHalfBath',
+# 'KitchenQual',
+# 'Functional',
+# 'GarageCars',
+# 'GarageArea',
+# 'SaleType']
+#
+#train NaN而test没有的
+#['Electrical']
+#'''
+#
+#missing_cols = missing_cols_train+missing_cols_test # 在训练集和测试集中有缺失值的列
+### 45个特征
+#train_features.drop(missing_cols,axis = 1,inplace = True)
+#test_features.drop(missing_cols,axis = 1,inplace = True)
 missing_cols_train = [col for col in train_features.columns
                      if train_features[col].isnull().any()]
-
-missing_cols_test = [col for col in test_features.columns
-                     if test_features[col].isnull().any()]
-
-##查找test和train中包含NaN的列有什么不同
-#[col for col in missing_cols_train if col not in missing_cols_test]
-
-missing_cols = missing_cols_train+missing_cols_test # 在训练集和测试集中有缺失值的列
-## 45个特征
-train_features.drop(missing_cols,axis = 1,inplace = True)
-test_features.drop(missing_cols,axis = 1,inplace = True)
-
+print('missing features:'+str(missing_cols_train))
 # 独热
 train_features = pd.get_dummies(train_features)
 test_features = pd.get_dummies(test_features)
+
+missing_cols_train = [col for col in train_features.columns
+                     if train_features[col].isnull().any()]
+print('missing features:'+str(missing_cols_train))
+
 #print("train_features's num: ",train_features.columns.size)
 #print("test_features's num: ",test_features.columns.size)
 #print("feature's name: ",[col for col in test_features.columns
@@ -51,7 +80,18 @@ test_features = pd.get_dummies(test_features)
 train_features,test_features = train_features.align(test_features,
                                                     join='left',
                                                     axis = 1)
-# 训练数据集分割成训练集和测试集，用于测试
+missing_cols_train = [col for col in train_features.columns
+                     if train_features[col].isnull().any()]
+print('missing features:'+str(missing_cols_train))
+#print(train_features.LotFrontage)
+# 缺失值处理
+my_imputer = Imputer(strategy = 'median')
+train_features = my_imputer.fit_transform(train_features)
+test_features = my_imputer.transform(test_features)
+#print(train_features.LotFrontage)
+##print("features num : "+len(train_features.columns))
+## 训练数据集分割成训练集和测试集，用于测试
+
 X_train, X_test, y_train, y_test = train_test_split(train_features, 
                                                     train_target,
                                                     train_size=0.8, 
